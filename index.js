d40cd76abbbf919d28aab3dddab6d107782b22b6
unknown <ghareeb_shehata@outlook.com>
2021-06-13 23:14:21 +0200

    initial commit for Keeper app backend

---
+++ index.js
@@ -0,0 +1,23 @@
+import express from "express";
+import cors from "cors";
+import mongoose from "mongoose";
+import 'dotenv/config';
+import userRoutes from "./routes/user.js"
+
+const uri = process.env.CONNECTION_URI;
+const PORT = process.env.PORT || 5000;
+const app = express();
+app.use(express.json());
+app.use(cors());
+app.use("/user", userRoutes)
+
+mongoose.connect(uri, {
+        useNewUrlParser: true,
+        useCreateIndex: true,
+        useUnifiedTopology: true
+    })
+    .then(() => app.listen(PORT, () => console.log("connected to Port " + PORT)))
+    .catch((error) => console.log(error));
+
+mongoose.set("useFindAndModify", false)
+mongoose.pluralize(null);
