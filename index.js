43dcf3ea52d13d772924676b74fc786f37094c5b
unknown <ghareeb_shehata@outlook.com>
2021-06-14 16:09:40 +0200

    handling sign up

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const PORT = process.env.PORT || 5000;
 const app = express();
 app.use(express.json());
 app.use(cors());
-app.use("/user", userRoutes)
+app.use("/users", userRoutes)
 
 mongoose.connect(uri, {
         useNewUrlParser: true,
