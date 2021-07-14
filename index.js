ad8312c96cb6289481adf13eec568ec0770688bc
unknown <ghareeb_shehata@outlook.com>
2021-07-14 11:38:15 +0200

    Fixing routing issue

--- index.js
+++ index.js
@@ -19,12 +19,12 @@ app.use("/reminder", reminderRoutes)
 app.use("/task", taskRoutes)
 
 
-if(process.env.NODE_ENV === 'production' || process.env.NODE_ENV === 'staging'){
-    app.use(express.static('client/build'))
-    app.get('*', (req, res)=>{
-        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
-    })
-}
+// if(process.env.NODE_ENV === 'production' || process.env.NODE_ENV === 'staging'){
+//     app.use(express.static('client/build'))
+//     app.get('*', (req, res)=>{
+//         res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+//     })
+// }
 
 mongoose.connect(uri, {
         useNewUrlParser: true,
